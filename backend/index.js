02c60fa87a2d30ba28f0a9c8cf4d998b299ea4ae
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-09-26 20:55:04 -0700

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { PORT } from './config.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
