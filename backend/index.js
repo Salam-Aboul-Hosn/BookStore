35b43d6b34156071c7a4bd7552367359f920b2d0
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-09-26 21:49:26 -0700

    Post route for /books

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,35 @@ import express from 'express';
 import { PORT } from './config.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
