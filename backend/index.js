e60a143832ef93d417bf6828ffcae5e5bde70665
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-09-26 21:13:41 -0700

    Book Model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { PORT } from './config.js';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
@@ -9,6 +10,14 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`Listening on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
