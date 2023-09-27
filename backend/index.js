c79b022da276da3c788aefb15edb1636ae488168
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-09-27 15:14:43 -0700

    Spinner component/set up

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/bookRoutes.js';
-import corse from 'corse';
+import cors from 'cors';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-
-app.use(
-  corse({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowHeaders: ['Content-Type'],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (req, res) => {
   res.send('hello');
